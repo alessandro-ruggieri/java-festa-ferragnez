653777cd8f8fc4e80cfd196d62892b59981b04e4
Alessandro Ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-11-13 01:15:12 +0100

    Update CheckGuest.java
    Added complete functionality with both "for" and "while".

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,9 +1,48 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
+		String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", 
+				"J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio",
+				"Luis", "Pardis Zarei", "Martina Maccherone", 
+				"Rachel Zeilic"};
+		String nomeInvitato = "";
+		System.out.println("Benvenuto a casa Ferragnez! Inserisci il tuo nome così che possa controllare la lista degli invitati.");
+		Scanner scan = new Scanner(System.in);
+		nomeInvitato = scan.nextLine();
+		Boolean isInvited = false; 
+		
+		/* metodo con il ciclo For
+		for (int i = 0; i < invitati.length; i++) {
+	
+			if (nomeInvitato.equalsIgnoreCase(invitati[i])) {
+				isInvited = true;
+				break;
+			}
+		}
+		*/
+		
+		// metodo con il while
+		int i = 0;
+		while (i < invitati.length) {
+			if (nomeInvitato.equalsIgnoreCase(invitati[i])) {
+				isInvited = true;
+				break;
+			}
+			i++;
+		}
+
+		if (isInvited == true) {
+			System.out.println("Ciao " + nomeInvitato + " , puoi entrare!");
+		}
+		else {
+			System.out.println("Mi dispiace " + nomeInvitato + " , non sei sulla lista e non puoi entrare!");
+		}
 		
 	}
 
 }
+
